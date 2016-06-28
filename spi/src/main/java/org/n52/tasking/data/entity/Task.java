415735e378ffe1e1ead27921c8676555ebfaf413
Henning Bredel <bredel@52north.org>
2016-06-28 15:32:23 +0200

    add interface for tasks

---
+++ spi/src/main/java/org/n52/tasking/data/entity/Task.java
@@ -0,0 +1,86 @@
+package org.n52.tasking.data.entity;
+
+public class Task {
+    
+    private String id;
+    
+    private String encodedParameters;
+    
+    private long estimatedToC;
+    
+    private String requestStatus;
+    
+    private long submittedAt;
+    
+    private double percentCompletion;
+    
+    private long updatedAt;
+    
+    private String resultId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEncodedParameters() {
+        return encodedParameters;
+    }
+
+    public void setEncodedParameters(String encodedParameters) {
+        this.encodedParameters = encodedParameters;
+    }
+
+    public long getEstimatedToC() {
+        return estimatedToC;
+    }
+
+    public void setEstimatedToC(long estimatedToC) {
+        this.estimatedToC = estimatedToC;
+    }
+
+    public String getRequestStatus() {
+        return requestStatus;
+    }
+
+    public void setRequestStatus(String requestStatus) {
+        this.requestStatus = requestStatus;
+    }
+
+    public long getSubmittedAt() {
+        return submittedAt;
+    }
+
+    public void setSubmittedAt(long submittedAt) {
+        this.submittedAt = submittedAt;
+    }
+
+    public double getPercentCompletion() {
+        return percentCompletion;
+    }
+
+    public void setPercentCompletion(double percentCompletion) {
+        this.percentCompletion = percentCompletion;
+    }
+
+    public long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getResultId() {
+        return resultId;
+    }
+
+    public void setResultId(String resultId) {
+        this.resultId = resultId;
+    }
+    
+    
+}
