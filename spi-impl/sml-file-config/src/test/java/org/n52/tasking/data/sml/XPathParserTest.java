2ac864bded7b092d66ca8ddbbed4ac1ab0612ac1
Henning Bredel <bredel@52north.org>
2016-06-28 14:53:04 +0200

    add some tests

---
+++ spi-impl/sml-file-config/src/test/java/org/n52/tasking/data/sml/XPathParserTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2016-2016 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of
+ * the following licenses, the combination of the program with the linked
+ * library is not considered a "derivative work" of the program:
+ *
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
+ *
+ * Therefore the distribution of the program linked with libraries licensed
+ * under the aforementioned licenses, is permitted by the copyright holders
+ * if the distribution is compliant with both the GNU General Public License
+ * version 2 and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+package org.n52.tasking.data.sml;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.xml.xpath.XPathExpressionException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.NodeList;
+
+public class XPathParserTest {
+
+    private static final String SML_CONFIG_FOLDER = "/files";
+
+    private XPathParser parser;
+
+    @Before
+    public void setUp() throws Exception {
+        parser = new XPathParser(getTestConfigFile("lisa-instance.xml"));
+    }
+
+    @Test
+    public void when_parserReady_then_parseNodeList() throws XPathExpressionException {
+        NodeList nodes = parser.parseNodes("/PhysicalComponent/identification");
+        assertThat(nodes.getLength(), is(greaterThan(0)));
+    }
+
+    @Test
+    public void when_parserReady_then_parseLisaIdentification() throws XPathExpressionException {
+        String id = parser.parseString("/PhysicalComponent/identifier");
+        assertThat(id, is("http://www.nexosproject.eu/resource/procedure/trios/lisa/1234567890"));
+    }
+
+    @Test
+    public void when_parserReady_then_parseUpdatableParameters() throws XPathExpressionException {
+        NodeList nodes = parser.parseNodes("/PhysicalComponent/parameters/ParameterList/parameter[@updatable='true']");
+        assertThat(nodes.getLength(), is(4));
+    }
+
+    private Path getTestConfigFolder() throws URISyntaxException {
+        return Paths.get(getClass().getResource(SML_CONFIG_FOLDER).toURI());
+    }
+
+    private File getTestConfigFile(String name) throws URISyntaxException {
+        Path folder = getTestConfigFolder();
+        return folder.resolve(name).toFile();
+    }
+
+}
---
+++ spi-impl/sml-file-config/src/test/java/org/n52/tasking/data/sml/device/SmlConfigDeviceRepositoryTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2016-2016 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of
+ * the following licenses, the combination of the program with the linked
+ * library is not considered a "derivative work" of the program:
+ *
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
+ *
+ * Therefore the distribution of the program linked with libraries licensed
+ * under the aforementioned licenses, is permitted by the copyright holders
+ * if the distribution is compliant with both the GNU General Public License
+ * version 2 and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+package org.n52.tasking.data.sml.device;
+
+import java.io.File;
+import org.n52.tasking.data.RepositoryConfigurationException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.n52.tasking.data.entity.Device;
+
+public class SmlConfigDeviceRepositoryTest {
+
+    private static final String SML_CONFIG_FOLDER = "/files";
+
+    private static final String LISA_INSTANCE_ID = "http://www.nexosproject.eu/resource/procedure/trios/lisa/1234567890";
+    
+    private SmlConfigDeviceRepository repository;
+
+    @Before
+    public void setUp() throws Exception {
+        File folder = getTestConfigFolder().toFile();
+        repository = new SmlConfigDeviceRepository(folder.getAbsolutePath());
+    }
+
+    @Test(expected = RepositoryConfigurationException.class)
+    public void when_configFolderNotPresentOnCreation_thenException() throws Exception {
+        repository = new SmlConfigDeviceRepository("does-not-exist");
+    }
+
+    @Test(expected = RepositoryConfigurationException.class)
+    public void when_givenFileOnCreation_thenException() throws Exception {
+        String file = getTestConfigFile("empty.txt").getAbsolutePath();
+        repository = new SmlConfigDeviceRepository(file);
+    }
+
+    @Test
+    public void when_xmlAvailable_then_notEmptyCollection() {
+        assertThat(repository.getDevices(), is(not(empty())));
+    }
+
+    @Test
+    public void when_configFolderRead_then_lisaInstanceAvailable() {
+        assertTrue(repository.hasDevice(LISA_INSTANCE_ID));
+    }
+    
+    private Path getTestConfigFolder() throws URISyntaxException {
+        return Paths.get(getClass().getResource(SML_CONFIG_FOLDER).toURI());
+    }
+
+
+    private File getTestConfigFile(String name) throws URISyntaxException {
+        Path folder = getTestConfigFolder();
+        return folder.resolve(name).toFile();
+    }
+
+}
