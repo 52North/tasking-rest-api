ceffc0d468f46318ffa6bbac1656b3a2f6eff1bd
Henning Bredel <bredel@52north.org>
2016-07-01 11:48:55 +0200

    orga imports

--- spi-impl/sml-file-config/src/test/java/org/n52/tasking/data/sml/device/PhysicalComponentLisaInstanceTest.java
+++ spi-impl/sml-file-config/src/test/java/org/n52/tasking/data/sml/device/PhysicalComponentLisaInstanceTest.java
@@ -35,9 +35,6 @@ import org.n52.tasking.data.entity.TaskingDescription;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class PhysicalComponentLisaInstanceTest extends AbstractDeviceParser {
 
